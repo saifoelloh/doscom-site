fc9d80b9cea14822d82610f349ecf28148ab6866
saifoelloh <saifoelloh@gmail.com>
2020-02-12 09:33:37 +0700

    adding some rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,8 @@ module.exports = {
   plugins: ['react'],
   rules: {
     semi: 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': 0,
   },
 }
