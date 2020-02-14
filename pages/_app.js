189c4ee35fee7f67342065765eed982a4cd23c28
saifoelloh <saifoelloh@gmail.com>
2020-02-15 00:23:57 +0700

    adding main css into project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import '../public/css/index.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
