7bc0aa6dae80a1c63c09c12bd1e56e3ff1396d78
saifoelloh <saifoelloh@gmail.com>
2020-04-28 22:37:46 +0700

    major update

---
+++ next.config.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+
+module.exports = {
+  env: {
+    API_KEY: process.env.API_KEY,
+    API_URL: process.env.API_URL,
+  },
+}
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Container } from "@material-ui/core"
+import React from "react"
+import Slider from "react-slick"
 
-const About = () => (
-  <fragment>
-    <h1>About</h1>
-  </fragment>
-)
+const About = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+  }
+  return (
+    <fragment>
+      <h1>About</h1>
+      <Container style={{ backgroundColor: "grey" }}>
+        <Slider {...settings}>
+          {[0, 1, 2, 3, 4, 5, 6].map((id) => {
+            return (
+              <div>
+                <h1>{id}</h1>
+              </div>
+            )
+          })}
+        </Slider>
+      </Container>
+    </fragment>
+  )
+}
 
 export default About
