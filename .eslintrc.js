8ae216c1f0a765443e6726071598b547f3ad2f6d
saifoelloh <saifoelloh@gmail.com>
2020-04-28 22:28:13 +0700

    adding some good stuf

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,28 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['react'],
+  plugins: ["react"],
   rules: {
     semi: 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/prop-types': 0,
-    'react/jsx-filename-extension': 0,
+    "react/jsx-props-no-spreading": 0,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": 0,
+    quotes: "double",
   },
 }
