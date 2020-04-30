c65b2579b5d9eab4ce61c453b6492ea4755c1675
saifoelloh <saifoelloh@gmail.com>
2020-04-30 23:13:54 +0700

    implement card-event in landing-page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
+import { Container } from "@material-ui/core"
+import React from "react"
+
+import CardEvent from "../components/landing-page/card-event.jsx"
+
 const Index = (props) => {
-  return <h1>Hello</h1>
+  return (
+    <Container>
+      <h1>Hello</h1>
+      {[...Array(5).keys()].map((item) => (
+        <CardEvent key={item} />
+      ))}
+    </Container>
+  )
 }
 
 export default Index
