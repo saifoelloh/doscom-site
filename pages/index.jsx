5851a91595b98d48a4ac6f41e03c98cdca5300d0
saifoelloh <saifoelloh@gmail.com>
2020-05-02 23:14:02 +0700

    adding landing page section

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,30 @@
 import { Container } from "@material-ui/core"
 import React from "react"
+import Slider from "react-slick"
 
 import CardEvent from "../components/landing-page/card-event.jsx"
+import { SectionAbout } from "../components/landing-page/section"
 
 const Index = (props) => {
+  const settings = {
+    accessibility: true,
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+  }
   return (
-    <Container>
-      <h1>Hello</h1>
-      {[...Array(5).keys()].map((item) => (
-        <CardEvent key={item} />
-      ))}
-    </Container>
+    <main>
+      <Container>
+        <h1>Hello</h1>
+        <SectionAbout />
+        <Slider {...settings}>
+          {[...Array(5).keys()].map((item) => (
+            <CardEvent key={item} />
+          ))}
+        </Slider>
+      </Container>
+    </main>
   )
 }
 
