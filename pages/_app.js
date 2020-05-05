fec653b3f3ad3a3511817d43371a41f3874e63ec
saifoelloh <saifoelloh@gmail.com>
2020-05-05 23:44:19 +0700

    adding custom css and react-slick css

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,9 @@
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import React from "react"
 import { Provider } from "react-redux"
 
 import store from "../redux/store"
-
-import "../public/css/custom.css"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
 
 export default function MyApp({ Component, pageProps }) {
   return (
