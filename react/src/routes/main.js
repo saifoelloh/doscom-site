1680a87e49e5b21cc590d48a3442dc00bd4f8974
saifoelloh <saifoelloh@gmail.com>
2020-05-09 23:34:11 +0700

    adding dashboard to main route

--- react/src/routes/main.js
+++ react/src/routes/main.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { Route, Switch } from "react-router-dom"
 
+import { Login } from "../pages/auth"
+import { PrivateRoute } from "../pages/auth/components"
 import { NotFound } from "../pages/error"
 import { LandingPage } from "../pages/landing-page"
-import { Login } from "../pages/auth"
+import { DashboardLayout } from "../pages/dashboard/components"
 
 const MainRoutes = () => {
   return (
     <Switch>
       <Route path="/" component={LandingPage} exact />
       <Route path="/auth/login" component={Login} />
+      <PrivateRoute exact path="/dashboard" component={DashboardLayout} />
       <Route path="*" component={NotFound} />
     </Switch>
   )
