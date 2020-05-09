ee0b1e3bbc4aeea41f8447940ccf2a3d1d350456
saifoelloh <saifoelloh@gmail.com>
2020-05-09 23:32:21 +0700

    desctructuring context

--- react/src/App.js
+++ react/src/App.js
@@ -1,36 +1,16 @@
-import React, { createContext, useReducer } from "react"
+import React from "react"
 import { BrowserRouter } from "react-router-dom"
 
+import { AuthProvider } from "./auth-context.js"
 import { MainRoutes } from "./routes"
 
-const initialState = {
-  name: "",
-  email: "",
-  isAuthenticate: false,
-}
-
-const AuthContext = createContext()
-
-const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      return {
-        ...state,
-        isAuthenticate: true,
-      }
-    default:
-      return state
-  }
-}
-
 function App() {
-  const [state, dispatch] = useReducer(authReducer, initialState)
   return (
-    <AuthContext.Provider value={[state, dispatch]}>
+    <AuthProvider>
       <BrowserRouter>
         <MainRoutes />
       </BrowserRouter>
-    </AuthContext.Provider>
+    </AuthProvider>
   )
 }
 
