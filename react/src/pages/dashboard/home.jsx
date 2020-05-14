bffdad7ba6193c12aa88e884d2e79bb63e333f2a
saifoelloh <saifoelloh@gmail.com>
2020-05-14 22:26:26 +0700

    adding dashboard home page

---
+++ react/src/pages/dashboard/home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Card, CardBody, CardHeader } from "reactstrap"
+
+const Dashboard = () => {
+  return (
+    <Card>
+      <CardHeader>Dashboard</CardHeader>
+      <CardBody>
+        <h1>Hello Dashboard</h1>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Dashboard
