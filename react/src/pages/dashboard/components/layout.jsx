13ace350c9e8f2de697cd4810772a99d157c89cb
saifoelloh <saifoelloh@gmail.com>
2020-05-14 22:26:01 +0700

    adding breadcrumb to layout

--- react/src/pages/dashboard/components/layout.jsx
+++ react/src/pages/dashboard/components/layout.jsx
@@ -1,31 +1,76 @@
 import React from "react"
-import { Col, Container, ListGroup, ListGroupItem, Row } from "reactstrap"
+import { useLocation } from "react-router-dom"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Row,
+} from "reactstrap"
 
 import DashboardRoutes from "../../../routes/dashboard.js"
 import { Link as RouteLink } from "react-router-dom"
 
 const DashboardLayout = () => {
+  const location = useLocation()
+  const paths = location.pathname.split("/")
+  const menus = [
+    {
+      title: "Dashboard",
+      url: "/dashboard/",
+    },
+    {
+      title: "User",
+      url: "/dashboard/user",
+    },
+    {
+      title: "Post",
+      url: "/dashboard/post",
+    },
+  ]
+
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={4}>
+    <Row className="mx-0 h-100 align-items-stretch">
+      <Col xs={12} md={2} className="h-100 bg-info p-0">
+        <Card className="h-100 border-0">
           <ListGroup>
-            <ListGroupItem tag={RouteLink} href="/dashboard" action>
-              Dashboard
-            </ListGroupItem>
-            <ListGroupItem tag={RouteLink} href="/user" action>
-              User
-            </ListGroupItem>
-            <ListGroupItem tag={RouteLink} href="/post" action>
-              Posts
-            </ListGroupItem>
+            {menus.map((menu, id) => (
+              <ListGroupItem
+                key={id}
+                tag={RouteLink}
+                to={menu.url}
+                active={location.pathname === menu.url}
+                action
+              >
+                {menu.title}
+              </ListGroupItem>
+            ))}
           </ListGroup>
-        </Col>
-        <Col xs={12} md={8}>
-          <DashboardRoutes />
-        </Col>
-      </Row>
-    </Container>
+        </Card>
+      </Col>
+      <Col xs={12} md={10}>
+        <Breadcrumb tag="nav" listTag="div">
+          {paths.map((path, id) => {
+            const url =
+              id === 0
+                ? `/${path}/`
+                : `/${paths.splice(0, id).join("/")}/${path}`
+            return location.pathname === url ? (
+              <BreadcrumbItem key={id} active>
+                {`${path} | ${paths.length}`}
+              </BreadcrumbItem>
+            ) : (
+              <BreadcrumbItem key={id} tag={RouteLink} to={url}>
+                {path}
+              </BreadcrumbItem>
+            )
+          })}
+        </Breadcrumb>
+        <DashboardRoutes />
+      </Col>
+    </Row>
   )
 }
 
