f114900c0a6f9e70911ac8e7e102050143f35266
saifoelloh <saifoelloh@gmail.com>
2020-05-18 23:01:02 +0700

    create dashboard header component

---
+++ react/src/pages/dashboard/components/header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import {
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarToggler,
+} from "reactstrap"
+
+const DashboardHeader = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/auth/login">Logout</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default DashboardHeader
