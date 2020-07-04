0dde29b5f46687609259e23cf270d2319da0cd64
saifoelloh <saifoelloh@gmail.com>
2020-07-04 23:28:20 +0700

    implement member page on dashboard

--- react/src/routes/dashboard.js
+++ react/src/routes/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, useRouteMatch } from 'react-router-dom'
 
 import { Dashboard } from '../pages/dashboard'
+import { MemberHome } from '../pages/dashboard/member'
 import PrivateRoute from '../pages/auth/components/private-route'
 
 const DashboardRoutes = () => {
@@ -14,7 +15,7 @@ const DashboardRoutes = () => {
     },
     {
       path: `${path}/user`,
-      component: () => <h1>user</h1>,
+      component: () => <MemberHome />,
       exact: true,
     },
     {
