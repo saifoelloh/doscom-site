c8de637e331b471533b002fed070a1e3a66dbb31
saifoelloh <saifoelloh@gmail.com>
2020-07-04 23:26:48 +0700

    create error page

---
+++ react/src/pages/error/index.js
@@ -0,0 +1,4 @@
+import InternalServerError from "./internal-server-error"
+import NotFound from "./not-found"
+
+export { NotFound, InternalServerError }
---
+++ react/src/pages/error/internal-server-error.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const InternalServerError = () => {
+  return <h1>500 | Server Error</h1>
+}
+
+export default InternalServerError
