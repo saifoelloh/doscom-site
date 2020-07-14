2274e7e34d88b04396610f631b8671ad26573ff9
saifoelloh <saifoelloh@gmail.com>
2020-07-14 23:55:15 +0700

    destructuring code

--- react/src/routes/main.js
+++ react/src/routes/main.js
@@ -1,18 +1,47 @@
-import React from "react"
-import { Route, Switch } from "react-router-dom"
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 
-import { Login } from "../pages/auth"
-import { PrivateRoute } from "../pages/auth/components"
-import { NotFound } from "../pages/error"
-import { LandingPage } from "../pages/landing-page"
-import { DashboardLayout } from "../pages/dashboard/components"
+import { Login } from '../pages/auth'
+import { PrivateRoute } from '../pages/auth/components'
+import { NotFound } from '../pages/error'
+import { LandingPage } from '../pages/landing-page'
+import { DashboardLayout } from '../pages/dashboard/components'
 
 const MainRoutes = () => {
+  const routes = [
+    {
+      path: '/',
+      component: LandingPage,
+      exact: true,
+    },
+    {
+      path: '/auth/login',
+      component: Login,
+    },
+    {
+      path: '/dashboard',
+      component: DashboardLayout,
+      exact: true,
+      private: true,
+    },
+  ]
   return (
     <Switch>
-      <Route path="/" component={LandingPage} exact />
-      <Route path="/auth/login" component={Login} />
-      <PrivateRoute path="/dashboard" component={DashboardLayout} />
+      {routes.map((route, index) =>
+        route.private === undefined ? (
+          <Route
+            path={route.path}
+            component={route.component}
+            exact={route?.exact}
+          />
+        ) : (
+          <PrivateRoute
+            path={route.path}
+            component={route.component}
+            exact={route?.exact}
+          />
+        ),
+      )}
       <Route path="*" component={NotFound} />
     </Switch>
   )
